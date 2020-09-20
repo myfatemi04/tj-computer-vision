f9e748ff18c178beea76243ec852a0d9374db089
Michael Fatemi <myfatemi04@gmail.com>
2020-09-20 12:35:54 -0400

    Added circumcircle code, out-of-boundaries catchers

--- Project 1 - Drawing/l01.cpp
+++ Project 1 - Drawing/l01.cpp
@@ -6,6 +6,10 @@
 
 using namespace std;
 
+struct Circle {
+    double x, y, radius;
+};
+
 void drawBresenhams(int*** pixels, int x1, int y1, int x2, int y2, int* color) {
     cout << "Drawing line connecting (" << x1 << ", " << y1 << ") and (" << x2 << ", " << y2 << ").\n";
 
@@ -77,8 +81,15 @@ void drawBresenhams(int*** pixels, int x1, int y1, int x2, int y2, int* color) {
     }
 }
 
-void drawCircle(int*** pixels, int center_x, int center_y, int radius, int* color) {
-    cout << "Drawing a circle centered at (" << center_x << ", " << center_y << ") with radius " << radius;
+void putPixel(int*** pixels, int* color, int x, int y, int width, int height) {
+    if (x < 0 || x >= width) return;
+    if (y < 0 || y >= height) return;
+
+    pixels[x][y] = color;
+}
+
+void drawCircle(int*** pixels, int center_x, int center_y, double radius, int* color) {
+    cout << "Drawing a circle centered at (" << center_x << ", " << center_y << ") with radius " << radius << "\n";
 
     int x, y, max_x, y2, y2_new, two_y;
 
@@ -87,7 +98,7 @@ void drawCircle(int*** pixels, int center_x, int center_y, int radius, int* colo
 
     // starts with the topmost point
     x = 0;
-    y = radius;
+    y = (int) radius;
 
     y2 = y * y;
     y2_new = y2;
@@ -105,15 +116,15 @@ void drawCircle(int*** pixels, int center_x, int center_y, int radius, int* colo
         }
 
         // Set all symmetric points to 1
-        pixels[x + center_x][y + center_y] = color;
-        pixels[-x + center_x][y + center_y] = color;
-        pixels[x + center_x][-y + center_y] = color;
-        pixels[-x + center_x][-y + center_y] = color;
+        putPixel(pixels, color, x + center_x, y + center_y, 200, 200);
+        putPixel(pixels, color, x + center_x, -y + center_y, 200, 200);
+        putPixel(pixels, color, -x + center_x, y + center_y, 200, 200);
+        putPixel(pixels, color, -x + center_x, -y + center_y, 200, 200);
 
-        pixels[y + center_x][x + center_y] = color;
-        pixels[-y + center_x][x + center_y] = color;
-        pixels[y + center_x][-x + center_y] = color;
-        pixels[-y + center_x][-x + center_y] = color;
+        putPixel(pixels, color, y + center_x, x + center_y, 200, 200);
+        putPixel(pixels, color, y + center_x, -x + center_y, 200, 200);
+        putPixel(pixels, color, -y + center_x, x + center_y, 200, 200);
+        putPixel(pixels, color, -y + center_x, -x + center_y, 200, 200);
 
         y2_new -= 2 * x - 3;
 
@@ -125,7 +136,7 @@ double distance(double x1, double y1, double x2, double y2) {
     return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
 }
 
-double* findIncircle(double ax, double ay, double bx, double by, double cx, double cy) {
+Circle findIncircle(double ax, double ay, double bx, double by, double cx, double cy) {
     double A = distance(bx, by, cx, cy);
     double B = distance(ax, ay, cx, cy);
     double C = distance(bx, by, ax, ay);
@@ -135,18 +146,118 @@ double* findIncircle(double ax, double ay, double bx, double by, double cx, doub
     double area = sqrt((s - A) * (s - B) * (s - C) / s);
     double inradius = area;
 
+    Circle myCircle;
+
     double ox = (A * ax + B * bx + C * cx) / p;
     double oy = (A * ay + B * by + C * cy) / p;
 
-    double* incircle = new double[3];
+    myCircle.x = ox;
+    myCircle.y = oy;
+    myCircle.radius = inradius;
 
-    incircle[0] = ox;
-    incircle[1] = oy;
-    incircle[2] = inradius;
+    return myCircle;
+}
 
-    cout << ox << " " << oy << " " << inradius << "\n";
+double* solveLinear(double a1, double b1, double c1, double a2, double b2, double c2) {
+    // matrix is:
+    /*
+    | a1 b1 | = | c1 |
+    | a2 b2 |   | c2 |
 
-    return incircle;
+    We will use Cramer's rule to find the (x, y) of the intersection.
+    */
+
+    double det = a1 * b2 - b1 * a2;
+
+    if (det == 0) {
+        throw 0;
+    }
+
+    double detX = c1 * b2 - b1 * c2;
+    double detY = a1 * c2 - c1 * a2;
+
+    double* solution = new double[2] { detX / det, detY / det };
+
+    return solution;
+}
+
+// This function uses linear equations to find the circumcircle
+// Circumcircle = intersection of perpendicular bisectors
+Circle findCircumcircle(double ax, double ay, double bx, double by, double cx, double cy) {
+    // find midpoints
+    double ab_x = (ax + bx) / 2;
+    double ab_y = (ay + ay) / 2;
+    double ac_x = (ax + cx) / 2;
+    double ac_y = (ay + cy) / 2;
+
+    /*
+    equations are ax + by = c
+    how to find a, b, and c?
+    y = mx + b
+    m = slope_numer/slope_denom
+    y = slope_numer/slope_denom * x + b
+    slope_denom(y) = slope_numer(x) + b
+    -slope_numer(x) + slope_denom(y) = b
+    now, substitute a point for (x, y) to find b. we can just use (x1, y1)
+    b = -slope_numer(x1) + slope_denom(y1)
+    now, we know that in ax + by = c:
+    a = -slope_numer
+    b = slope_denom
+    c = -a(x1) + b(y1)
+    */
+    
+    cout << "(" << ax << ", " << ay << ") (" << bx << ", " << by << ") (" << cx << ", " << cy << ")\n";
+
+    // the slope of AB = (y2 - y1) / (x2 - x1)
+    // to find the perpendicular slope, we do -(x2 - x1) / (y2 - y1)
+
+    // find a, b, and c of first side
+    double ab_slope_numer = -(bx - ax);
+    double ab_slope_denom = (by - ay);
+
+    // cout << "AB slope numer: " << ab_slope_numer << endl;
+    // cout << "AB slope denom: " << ab_slope_denom << endl;
+
+    double ab_a = -ab_slope_numer;
+    double ab_b = ab_slope_denom;
+    double ab_c = ab_a * ab_x + ab_b * ab_y;
+
+    // find a, b, and c of second side
+    double ac_slope_numer = -(cx - ax);
+    double ac_slope_denom = (cy - ay);
+
+    double ac_a = -ac_slope_numer;
+    double ac_b = ac_slope_denom;
+    double ac_c = ac_a * ac_x + ac_b * ac_y;
+
+    // solve the linear equation for the center
+    double circumcenter_x = 0;
+    double circumcenter_y = 0;
+
+    try {
+        double* circumcenter = solveLinear(ab_a, ab_b, ab_c, ac_a, ac_b, ac_c);
+        
+        circumcenter_x = circumcenter[0];
+        circumcenter_y = circumcenter[1];
+    } catch (int i) {
+        if (i == 0) {
+            // this means there is no solution (the lines are parallel)
+            // the only way for the lines to be parallel in a triangle is
+            // if the lines are on top of each other
+            
+            // so, we can just use a midpoint
+            circumcenter_x = ab_x;
+            circumcenter_y = ab_y;
+        }
+    }
+
+    // Now, we must find the radius. We will simply find the distance
+    // from the circumcenter to one of the points.
+    double radius = distance(circumcenter_x, circumcenter_y, ax, ay);
+
+    Circle myCircle{circumcenter_x, circumcenter_y, radius};
+
+    return myCircle;
 }
 
 void printArray(int*** array, ostream& out, int width, int height, int channels = 3) {
@@ -245,11 +356,19 @@ int main() {
     myArray[x3_scale][y3_scale] = PURPLE;
 
     // Draw incircle
-    double* incircle = findIncircle(x1, y1, x2, y2, x3, y3);
+    Circle incircle = findIncircle(x1, y1, x2, y2, x3, y3);
     drawCircle(myArray,
-        scale(incircle[0], width), // x
-        scale(incircle[1], height), // y
-        scale(incircle[2], width), // radius
+        scale(incircle.x, width), // x
+        scale(incircle.y, height), // y
+        incircle.radius * width, // radius
+        WHITE);
+
+    // Draw circumcircle
+    Circle circumcircle = findCircumcircle(x1, y1, x2, y2, x3, y3);
+    drawCircle(myArray,
+        scale(circumcircle.x, width), // x
+        scale(circumcircle.y, height), // y
+        circumcircle.radius * width, // radius
         WHITE);
 
     // save the image
