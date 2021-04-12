91e271344c5c6a3d077b8b6caaf24ec2f514c007
Michael Fatemi <myfatemi04@gmail.com>
2021-04-12 14:49:10 -0400

    Update to use new files

--- Labs/conv_l061.bat
+++ Labs/conv_l061.bat
@@ -1,3 +1,7 @@
-wsl convert houghcircles_color_output.ppm houghcircles_color_output.png
-wsl convert houghcircles_edges_output.ppm houghcircles_edges_output.png
-wsl convert houghcircles_votes_output.ppm houghcircles_votes_output.png
+wsl convert imagev.ppm imagev.png
+wsl convert imagef.ppm imagef.png
+wsl convert imageCC.ppm imageCC.png
+
+@REM wsl convert houghcircles_color_output.ppm houghcircles_color_output.png
+@REM wsl convert houghcircles_edges_output.ppm houghcircles_edges_output.png
+@REM wsl convert houghcircles_votes_output.ppm houghcircles_votes_output.png
