4bbc860e5e78650f3c8919f4a45bdba553d81d9f
Michael Fatemi <myfatemi04@gmail.com>
2021-05-12 14:48:58 -0400

    convert to class-based

--- OpenCV/l072.cpp
+++ OpenCV/l072.cpp
@@ -2,22 +2,38 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
-typedef struct {
-	double x, y;
+class Location2 {
+	private:
+		double x, y;
 
-	cv::Point toPoint() const {
-		return cv::Point(x, y);
-	}
+	public:
+		Location2(double x, double y): x(x), y(y) {}
 
-	cv::Point toCenteredPoint(const cv::MatSize& size) const {
-		int width = size[1];
-		int height = size[0];
-		int centerX = width / 2;
-		int centerY = height / 2;
-		int projectionToImageScalar = (height / 4);
-		return cv::Point((x * projectionToImageScalar) + centerX, (y * projectionToImageScalar) + centerY);
-	}
-} Location2;
+		double getX() const {
+			return x;
+		}
+		
+		double getY() const {
+			return y;
+		}
+
+		Location2 scale(double amount) const {
+			return Location2 { x * amount, y * amount };
+		}
+
+		cv::Point toPoint() const {
+			return cv::Point(x, y);
+		}
+
+		cv::Point toCenteredPoint(const cv::MatSize& size) const {
+			int width = size[1];
+			int height = size[0];
+			int centerX = width / 2;
+			int centerY = height / 2;
+			int projectionToImageScalar = (height / 4);
+			return cv::Point((x * projectionToImageScalar) + centerX, (y * projectionToImageScalar) + centerY);
+		}
+};
 
 typedef double Orientation2;
 
@@ -26,9 +42,71 @@ typedef struct {
 	Orientation2 orientation;
 } Position2;
 
-typedef struct {
-	double x, y, z;
-} Location3;
+class Location3 {
+	private:
+		double x, y, z;
+
+	public:
+		Location3(): x(0), y(0), z(0) {}
+
+		Location3(double x, double y, double z): x(x), y(y), z(z) {}
+
+		double getX() const {
+			return x;
+		}
+
+		double getY() const {
+			return y;
+		}
+
+		double getZ() const {
+			return z;
+		}
+
+		void rotateAroundZ(double radians) {
+			x = cos(radians) * x - sin(radians) * y;
+			y = sin(radians) * x + cos(radians) * y;
+		}
+
+		void translate(const Location3& amount) {
+			x += amount.x;
+			y += amount.y;
+			z += amount.z;
+		}
+
+		std::string toVectorString() const {
+			std::stringstream s;
+			s << '<' << x << ", " << y << ", " << z << '>';
+			return s.str();
+		}
+		
+		std::string toPointString() const {
+			std::stringstream s;
+			s << '(' << x << ", " << y << ", " << z << ')';
+			return s.str();
+		}
+
+		Location3 vectorTo(const Location3& other) const {
+			return Location3 {
+				other.x - x,
+				other.y - y,
+				other.z - z
+			};
+		}
+
+		Location3 rotatedAroundZ(double rotation) const {
+			double cosTheta = cos(rotation);
+			double sinTheta = sin(rotation);
+
+			Location3 rotated {
+				cosTheta * x - sinTheta * y,
+				sinTheta * x + cosTheta * y,
+				z
+			};
+
+			return rotated;
+		}
+};
 
 typedef double Orientation3;
 
@@ -37,10 +115,41 @@ typedef struct {
 	Orientation3 orientation;
 } Position3;
 
-typedef struct {
-	Position3 position;
-	double sideLength;
-} Cube;
+class Cube {
+	private:
+		Position3 position;
+		double sideLength;
+
+	public:
+		Cube(Position3 position, double sideLength): position(position), sideLength(sideLength) {}
+		
+		Location3 *getVertices() const {
+			double scaleFactor = sideLength / 2;
+			double rotation = position.orientation;
+
+			auto locations = new Location3[8];
+			for (int i = 0; i < 8; i++) {
+				// Points are ordered by ZYX in bits
+				int x = (i >> 2) & 1;
+				int y = (i >> 1) & 1;
+				int z = (i >> 0) & 1;
+
+				auto vertex = Location3 {
+					(x * sideLength) - scaleFactor,
+					(y * sideLength) - scaleFactor,
+					(z * sideLength) - scaleFactor
+				};
+
+				vertex.rotateAroundZ(rotation);
+
+				vertex.translate(position.location);
+
+				locations[i] = vertex;
+			}
+
+			return locations;
+		}
+};
 
 typedef struct {
 	Position3 position;
@@ -54,63 +163,18 @@ typedef struct {
 	VertexID b;
 } Edge;
 
-// Returns an array of eight vertices.
-Location3 *getVerticesOfCube(const Cube& cube) {
-	double scaleFactor = cube.sideLength / 2;
-	double rotation = cube.position.orientation;
-
-	auto locations = new Location3[8];
-	for (int i = 0; i < 8; i++) {
-		// Points are ordered by YZX in bits
-		int x = (i >> 0) & 1;
-		int z = (i >> 1) & 1;
-		int y = (i >> 2) & 1;
-
-		locations[i] = Location3 {
-			(x * cube.sideLength) - scaleFactor,
-			(y * cube.sideLength) - scaleFactor,
-			(z * cube.sideLength) - scaleFactor
-		};
-	}
-
-	return locations;
-}
-
-Location3 getVectorFromAToB(const Location3& b, const Location3& a) {
-	return {
-		b.x - a.x,
-		b.y - a.y,
-		b.z - a.z
-	};
-}
-
-// Rotates the vector around the Z axis.
-Location3 getVectorRotatedAroundZ(const Location3& a, double rotation) {
-	double cosTheta = cos(rotation);
-	double sinTheta = sin(rotation);
-
-	Location3 rotated {
-		cosTheta * a.x - sinTheta * a.y,
-		sinTheta * a.x + cosTheta * a.y,
-		a.z
-	};
-
-	return rotated;
-}
 
 Location3 getVectorFromPerspective(const Location3& point, const Position3& perspective) {
 	std::cout << "Getting vector from location to point\n";
 	// Get the vector from the viewer to the point
-	auto relativeVector = getVectorFromAToB(perspective.location, point);
+	auto relativeVector = perspective.location.vectorTo(point);
 
-	std::cout << "Relative vector: ";
-	std::cout << "<" << relativeVector.x << ", " << relativeVector.y << ", " << relativeVector.z << ">";
-	std::cout << '\n';
+	std::cout << "Relative vector: " << relativeVector.toVectorString() << '\n';
 	
 	std::cout << "Rotating vector around Z\n";
 	// Then, rotate it by the viewer's rotation
 	auto zRotation = perspective.orientation;
-	auto rotatedRelativeVector = getVectorRotatedAroundZ(relativeVector, -zRotation);
+	auto rotatedRelativeVector = relativeVector.rotatedAroundZ(-zRotation);
 
 	return rotatedRelativeVector;
 }
@@ -122,27 +186,23 @@ Location2 getProjectedLocation(const Location3& point, const Camera3& camera) {
 	// We simply multiply the distance to the projection plane by the X and Y
 	// components of the relative vector, scaled so that Z = -1.
 	auto vectorFromPerspectiveScaled = Location2 {
-			vectorFromPerspective.x / vectorFromPerspective.z,
-			vectorFromPerspective.y / vectorFromPerspective.z
-	};
-	auto projectionPlaneDistance = camera.projectionPlaneDistance;
-	auto u = vectorFromPerspectiveScaled.x * projectionPlaneDistance;
-	auto v = vectorFromPerspectiveScaled.y * projectionPlaneDistance;
-	
-	Location2 projectedLocation {
-		u, v
-	};
+			vectorFromPerspective.getX(),
+			vectorFromPerspective.getY()
+	}.scale(1 / vectorFromPerspective.getZ());
 
-	std::cout << "Projected location for ";
-	std::cout << "(" << point.x << ", " << point.y << ", " << point.z << ")";
-	std::cout << " is at (" << u << ", " << v << ")\n";
+	auto projectionPlaneDistance = camera.projectionPlaneDistance;
+
+	auto projectedLocation = vectorFromPerspectiveScaled.scale(projectionPlaneDistance);
+
+	std::cout << "Projected location for " << point.toPointString();
+	std::cout << " is at (" << projectedLocation.getX() << ", " << projectedLocation.getY() << ")\n";
 
 	return projectedLocation;
 }
 
 void renderCube(cv::Mat& out, const Camera3& camera, const Cube& cube) {
 	std::cout << "Getting vertices of cube\n";
-	auto vertices = getVerticesOfCube(cube);
+	auto vertices = cube.getVertices();
 	auto edges = new Edge[12] {
 		{0, 1},
 		{1, 3},
@@ -193,7 +253,7 @@ int main() {
 	const int IMAGE_WIDTH = 1200;
 	const int IMAGE_HEIGHT = 800;
 	auto image = cv::Mat(cv::Size2i(IMAGE_WIDTH, IMAGE_HEIGHT), CV_8UC1);
-	auto cube = Cube { Position3 { Location3 { 0, 0, 0 }, 0 }, 1 };
+	auto cube = Cube(Position3 { Location3(0, 0, 0), 1 }, 1);
 	auto camera = Camera3 { Position3 { Location3 { 0, 0, -1 }, 0 }, 1 };
 
 	std::cout << "Rendering cube\n";
